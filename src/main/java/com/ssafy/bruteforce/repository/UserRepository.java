a8d35ff3366e82fb04998a7e579af945a8b26eae
CHOHYEONIN <kasitw1124@gmail.com>
2019-08-06 16:26:26 +0900

    ADD fuction findAllUserByName - @param name - @return List<User> Desc : 자신의 이름에 name을 포함하는 계정 리스트 반환 BY 조현인

--- src/main/java/com/ssafy/bruteforce/repository/UserRepository.java
+++ src/main/java/com/ssafy/bruteforce/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.bruteforce.repository;
 
+import java.util.List;
+
 import com.ssafy.bruteforce.dto.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User,String> {
+    List<User> findByNameLike(String name);
 }
