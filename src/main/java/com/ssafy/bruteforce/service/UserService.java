aae0d55bed39f040251ce34e53ec495712338280
HaeIn <wjdgodls0827@naver.com>
2019-08-07 16:16:38 +0900

    ADD ResultJson 리턴 타입 일관성있게 설정. Update UserService and Controller 예외처리를 service로 변경

--- src/main/java/com/ssafy/bruteforce/service/UserService.java
+++ src/main/java/com/ssafy/bruteforce/service/UserService.java
@@ -1,30 +1,29 @@
 package com.ssafy.bruteforce.service;
 
-import java.util.List;
 
+import com.ssafy.bruteforce.dto.ResultJson;
 import com.ssafy.bruteforce.dto.User;
-import com.ssafy.bruteforce.dto.UserQnAInfo;
 
 public interface UserService{
 
-    public void addUser(User user);
+    public ResultJson addUser(User user);
     
-    public void updateUser(User user);
+    public ResultJson updateUser(User user);
     
-    public void deleteUserById(String id);
+    public ResultJson deleteUserById(String id);
     
-    public void deactivateUserById(String id);
+    public ResultJson deactivateUserById(String id);
     
-    public List<User> findAllUserByName(String name);
+    public ResultJson findAllUserByName(String name);
     
     /** 회원 전체 검색 */
     // public List<User> searchAllUser(PageBean bean); // TODO:페이지 빈 구현
     
-    public boolean login(String id, String pw);
+    public ResultJson login(String id, String pw);
     
-    public boolean existsById(String id);
+    public ResultJson existsById(String id);
     
-	public User findUserById(String id);
+	public ResultJson findUserById(String id);
 
-    public List<UserQnAInfo> findAllUserQnAInfoById(String id);
+    public ResultJson findAllUserQnAInfoById(String id);
 }
