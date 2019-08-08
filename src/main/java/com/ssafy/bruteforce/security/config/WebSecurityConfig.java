d43823aaa89baefca76748759d2054cbc4679587
CHOHYEONIN <kasitw1124@gmail.com>
2019-08-08 10:24:16 +0900

    ADD SpringSecurity - WebSecurityConfig.java     * 권한에 따른 url 접근 설정 - UserAuthentication.java     * 유저의 권한 정보를 보유 - AuthProvider.java     * 유저에 권한을 부여

---
+++ src/main/java/com/ssafy/bruteforce/security/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+
+package com.ssafy.bruteforce.security.config;
+import com.ssafy.bruteforce.security.provider.AuthProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AuthProvider authProvider;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**","/login","/error**").permitAll()
+                .antMatchers("auth/admin/**").hasRole("ADMIN")
+                .antMatchers("auth/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("id")
+                .passwordParameter("password");
+            
+        http.logout()  
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+
+        http.authenticationProvider(authProvider);
+    }
+
+}
