9ae278396b8f7cd7a26512c9d284befd03ee6734
CHOHYEONIN <kasitw1124@gmail.com>
2019-08-13 11:42:07 +0900

    add docerk compose by 조현인

--- src/test/java/com/ssafy/bruteforce/ApplicationTests.java
+++ src/test/java/com/ssafy/bruteforce/ApplicationTests.java
@@ -18,38 +18,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
-    @Autowired
-    BoardService boardTest;
+    // @Autowired
+    // BoardService boardTest;
 
-    @Autowired
-    UserService userTest;
+    // @Autowired
+    // UserService userTest;
 
 	@Test
 	public void userServiceTest() {
-        User user1 = new User("1", "null", "null", "null", 0);
-        User user2 = new User("2", "null", "null", "null", 0);
-        User user3 = new User("3", "null", "null", "null", 0);
-        User user4 = new User("4", "null", "null", "null", 0);
-        System.out.println("=====user add test=====");
-        userTest.addUser(user1);
-        userTest.addUser(user2);
-        userTest.addUser(user3);
-        userTest.addUser(user4);
-        System.out.println("=====findAllUserByName test====");
-        System.out.println(userTest.findAllUserByName("nu"));
-        System.out.println("=====deleteUserById test====");
-        userTest.deleteUserById("1");
-        System.out.println(userTest.findAllUserByName("null"));
-        System.out.println("=====updateUser test====");
-        user4.setName("nasdf");
-        userTest.updateUser(user4);
-        userTest.deactivateUserById("2");
-        System.out.println(userTest.findAllUserByName("null"));
-        System.out.println("=====findUserById test====");
-        System.out.println(userTest.findUserById("4"));
-        userTest.deleteUserById("2");
-        userTest.deleteUserById("3");
-        userTest.deleteUserById("4");
+        // User user1 = new User("1", "null", "null", "null", 0);
+        // User user2 = new User("2", "null", "null", "null", 0);
+        // User user3 = new User("3", "null", "null", "null", 0);
+        // User user4 = new User("4", "null", "null", "null", 0);
+        // System.out.println("=====user add test=====");
+        // userTest.addUser(user1);
+        // userTest.addUser(user2);
+        // userTest.addUser(user3);
+        // userTest.addUser(user4);
+        // System.out.println("=====findAllUserByName test====");
+        // System.out.println(userTest.findAllUserByName("nu"));
+        // System.out.println("=====deleteUserById test====");
+        // userTest.deleteUserById("1");
+        // System.out.println(userTest.findAllUserByName("null"));
+        // System.out.println("=====updateUser test====");
+        // user4.setName("nasdf");
+        // userTest.updateUser(user4);
+        // userTest.deactivateUserById("2");
+        // System.out.println(userTest.findAllUserByName("null"));
+        // System.out.println("=====findUserById test====");
+        // System.out.println(userTest.findUserById("4"));
+        // userTest.deleteUserById("2");
+        // userTest.deleteUserById("3");
+        // userTest.deleteUserById("4");
     }
     
     @Test
@@ -76,6 +76,6 @@ public class ApplicationTests {
         // boardTest.deleteQuestionComment("5d4d1e4a61cbaa4e607bd9cf", "5d4d1e8461cbaa50e8d3c0d7");
         // boardTest.deleteAnswerComment("5d4d1e8461cbaa50e8d3c0d1", "5d4d1eb661cbaa3e98701cd4");
         // boardTest.closedUpdate("5d4d1e4a61cbaa4e607bd9cf", "5d4d1e8461cbaa50e8d3c0d1");
-        boardTest.findQuestionByTag(new int[] {0,0,0,1,0,1}, 2);
+        // boardTest.findQuestionByTag(new int[] {0,0,0,1,0,1}, 2);
     }
 }
